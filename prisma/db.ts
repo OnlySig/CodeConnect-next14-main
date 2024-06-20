200e92af2857265964473d490eb8a2b388278933
OnlySig <gabriel.castro.pereira11@gmail.com>
2024-06-20 18:16:07 -0300

    parametros de build da vercel 5

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,4 +1,17 @@
-import { PrismaClient } from '@prisma/client'
-const db = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
 
-export default db
+const db = new PrismaClient();
+
+async function connectToDatabase() {
+  try {
+    await db.$connect();
+    console.log("Conexão com o banco de dados estabelecida.");
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error);
+    process.exit(1);
+  }
+}
+
+connectToDatabase();
+
+export default db;
